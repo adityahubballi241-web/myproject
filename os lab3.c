2d23158bf976b72b8dc96e66812ceebe99bf5635
adityahubballi241-web <adityahubballi241@gmail.com>
2025-12-12 23:00:23 +0530

    Add files via upload

---
+++ os lab3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of two numbers is %d",c);
+    return 0;
+    
+}
